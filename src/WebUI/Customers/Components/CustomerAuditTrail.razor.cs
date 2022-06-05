b3356b530f5fa0e9a4ac9baa2d630ff1e8d2e19d
Erin McLaughlin <erinmclaughlin219@gmail.com>
2022-06-05 06:09:28 -0400

    moved audit trail into component

--- src/WebUI/Customers/Components/CustomerAuditTrail.razor.cs
+++ src/WebUI/Customers/Components/CustomerAuditTrail.razor.cs
@@ -30,37 +30,4 @@ public partial class CustomerAuditTrail
         AuditTrail = response.Content;
         StateHasChanged();
     }
-
-    private static string GetIcon(AuditType auditType, EventType eventType)
-    {
-        if (auditType == AuditType.Create)
-        {
-            return "text-success " + eventType switch
-            {
-                EventType.CustomerCreated => "fa-solid fa-user-plus",
-                EventType.WorkOrderCreated => "fa-solid fa-file-circle-plus",
-                _ => "fa-solid fa-plus"
-            };
-        }
-
-        if (auditType == AuditType.Update)
-        {
-            return "text-primary " + eventType switch
-            {
-                EventType.CustomerLocationUpdated => "fa-solid fa-earth-americas",
-                EventType.CustomerNameUpdated => "fa-solid fa-user-tag",
-                _ => "fa-solid fa-pencil-alt"
-            };
-        }
-
-        if (auditType == AuditType.Delete)
-        {
-            return "text-danger " + eventType switch
-            {
-                _ => "fa-solid fa-trash-alt"
-            };
-        }
-
-        return "fa-solid fa-plus text-muted";
-    }
 }
